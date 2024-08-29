5d895b6a2c9d19f7d4106885d5ec9f2c93e43520
Dan Azevedo <159485887+Azevedod1@users.noreply.github.com>
2024-08-29 19:19:24 +0000

    atualização no codigo

--- concat_dados.py
+++ concat_dados.py
@@ -3,4 +3,4 @@ info2 = input("Digite sua idade: ")
 
 info_concatenada = info1 + "  " + info2
 
-print("Olá, seu nome e idade são: ", info_concatenada)
+print("Olá, seu nome e idade são: ", info_concatenada "anos")
